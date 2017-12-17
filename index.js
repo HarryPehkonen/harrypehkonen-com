d303bd46a047e8b9e23704cef42f20ae1a52baeb
Harry Pehkonen <harry.pehkonen+git@gmail.com>
2017-12-17 17:36:56 +0000

    after letsencrypt

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-// const http = require("http2");
 const http = require("http");
-const https = require("https");
 const http2 = require("http2");
 const fs = require("fs");
 
@@ -9,12 +7,12 @@ const route = require("koa-route");
 const koaStatic = require("koa-static");
 const app = new Koa();
 
-app.use(async function(ctx, next) {
-  console.log("!!!");
-  await next();
-});
 app.use(route.get("/api/:whatever", everything));
-app.use(koaStatic(__dirname + "/static"));
+
+const staticOpts = {
+  hidden: true,
+};
+app.use(koaStatic(__dirname + "/static", staticOpts));
 app.use(badurl);
 
 async function everything(ctx, next) {
@@ -25,23 +23,19 @@ async function everything(ctx, next) {
 async function badurl(ctx, next) {
   console.log("badurl: " + ctx.request.originalUrl);
   console.log(JSON.stringify(ctx.request, null, 4));
-  ctx.throw(404, "Not found :D", "Not Found :S");
+  ctx.throw(404, "Not found", "Not Found");
 }
 
+const certpath = "/etc/letsencrypt/live/www.harrypehkonen.com";
 const httpsOptions = {
-  key: fs.readFileSync('./localhost.key'),
-  cert: fs.readFileSync('./localhost.cert'),
+  key: fs.readFileSync(certpath + "/privkey.pem"),
+  cert: fs.readFileSync(certpath + "/cert.pem"),
 };
 
-// http
-//   .createServer(app.callback())
-//   .listen(80)
-// ;
-
-// https
-//   .createServer(httpsOptions, app.callback())
-//   .listen(443)
-// ;
+http
+  .createServer(app.callback())
+  .listen(80)
+;
 
 http2
   .createSecureServer(httpsOptions, app.callback())
